c518dcc2b2ed2d6ab3011e2144afb9209c1fb413
Your Name <you@example.com>
2025-03-17 14:11:55 +0300

    added requirements

--- income_tracker/settings.py
+++ income_tracker/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5g%y46ys__^!goedxl+5wl649tnom1p550xw62w^ab8zzw!gpy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
 
 
 # Application definition
